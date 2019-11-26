c035004e04ed06276f4da452659c1bfef87d1e1e
Anthony <anthonylandry777@gmail.com>
2019-11-26 11:02:17 -0800

    committing all chenges

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Keys from './components/Keys';
+import Output from './components/Output';
+
+class App extends Component {
+  state = {
+    result: ""
+  };
+
+  buttonPressed = (e) => {
+    const buttonName = e.target.textContent
+    
+    if (buttonName === "=") {
+      this.calculate();
+      
+    } else if (buttonName === "C") {
+      this.reset();
+    } 
+
+    else if(buttonName === "Back") {
+      this.backspace();
+    }
+    
+    else {
+
+      this.setState({
+        result: this.state.result + buttonName
+        
+    });
+  };
+};
+
+
+
+  calculate = () => {
+    try {
+    this.setState ({
+      result: (eval(this.state.result) || "" )  + ""
+      });
+    } catch (e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+  
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  };
+
+
+  render () {
+    return (
+      <div>
+        <Output result={this.state.result}/>
+        <Keys buttonPressed={this.buttonPressed}/>
+      </div>
+  
+    );
+  }
 }
 
 export default App;
