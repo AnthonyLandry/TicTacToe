4b308bf1957740bd837b658fe311159ca85071fe
Anthony <anthonylandry777@gmail.com>
2019-11-29 15:18:13 -0800

    Updates

---
+++ precache-manifest.4ee16b8cd0c03a9dbfaf51e01a100dc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8ab5b9ff4f881044cd284b191cc2e0",
+    "url": "/The-calculator/index.html"
+  },
+  {
+    "revision": "706328ed840131d62bbc",
+    "url": "/The-calculator/static/css/main.3772c476.chunk.css"
+  },
+  {
+    "revision": "d537a8e22256cbff7db3",
+    "url": "/The-calculator/static/js/2.70872189.chunk.js"
+  },
+  {
+    "revision": "706328ed840131d62bbc",
+    "url": "/The-calculator/static/js/main.a6b13c85.chunk.js"
+  },
+  {
+    "revision": "d0161dd7256b9cacfd83",
+    "url": "/The-calculator/static/js/runtime~main.47850ca8.js"
+  }
+]);
